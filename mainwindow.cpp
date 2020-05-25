cd0999bacc71ae1430ec26d99984b3a00382e09a
Moodax <64415905+Moodax@users.noreply.github.com>
2020-05-25 09:21:06 +0200

    Početak
    Dodane tekstualne datoteke iz kojih se učitavaju imena momčadi(u kasnijim verzijama moguće da će biti binarne datoteke) te popravljen dizajn programa.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include<ctime>
+#include<math.h>
+#include<string>
+#include<stdlib.h>
+#include<fstream>
+#include<utility>
+#include<algorithm>
+#include<numeric>
+#include<iostream>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    ui->setupUi(this); 
 }
 
 MainWindow::~MainWindow()
@@ -13,10 +21,16 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
 void MainWindow::on_DALJE_clicked()
 {
+    int izbor;
+    static int a=0;
+    a++;
+    if(a==1)
+    {
+     izbor=ui->upis->text().toInt();
 
+    }
 }
 
 void MainWindow::on_prvaMomcad_clicked()
